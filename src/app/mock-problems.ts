0d08e320de259d2440188df939a580f490fe432f
sherrysack <1367368457@qq.com>
2018-07-31 16:31:26 +0800

    first commit of oj's client side code

---
+++ src/app/mock-problems.ts
@@ -0,0 +1,35 @@
+import { Problem } from './models/problem.model';
+
+export const PROBLEMS: Problem[] = [{
+  id: 1,
+  name: 'two sum',
+  desc: 'Given an array of integers, return indices of the two numbers such that they add up to a specific target.' +
+    'You may assume that each input would have exactly one solution, and you may not use the same element twice.',
+  difficulty: 'easy'
+},
+
+  {
+    id: 2,
+    name: 'Add Two numbers',
+    desc: 'Given an array of integers, return indices of the two numbers such that they add up to a specific target.' +
+      'You may assume that each input would have exactly one solution, and you may not use the same element twice.',
+    difficulty: 'medium'
+  },
+
+  {
+    id: 3,
+    name: 'Longest Substring Without Repeating Characters',
+    desc: 'Given a string, find the length of the longest substring without repeating characters.\n' +
+      '\n',
+    difficulty: 'hard'
+  },
+
+  {
+    id: 4,
+    name: 'Longest Substring Without Repeating Characters',
+    desc: 'Given a string, find the length of the longest substring without repeating characters.\n' +
+      '\n',
+    difficulty: 'hard'
+  }
+
+]
